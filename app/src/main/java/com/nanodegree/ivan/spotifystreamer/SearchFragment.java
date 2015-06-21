a820d4c81872574384608cdeb427a80563a6ea17
iruizeguilaz <iruizeguilaz@gmail.com>
2015-06-21 19:20:56 +0200

    Initial commit

---
+++ app/src/main/java/com/nanodegree/ivan/spotifystreamer/SearchFragment.java
@@ -0,0 +1,53 @@
+package com.nanodegree.ivan.spotifystreamer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        doSearch();
+        return rootView;
+
+    }
+
+    private void doSearch() {
+        final EditText et = (EditText)getActivity().findViewById(R.id.searchListSongs);
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = et.getText().toString().toLowerCase(Locale.getDefault());
+                // TODO lanzar busqueda lanzando la llamada AsyncTask
+            }
+        });
+    }
+
+
+}
