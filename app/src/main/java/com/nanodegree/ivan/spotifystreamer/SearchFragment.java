33148342d15cc22b5b5c8405821999fb855b45dc
iruizeguilaz <iruizeguilaz@gmail.com>
2015-06-22 23:19:17 +0200

    searchable onfocuschanged

--- app/src/main/java/com/nanodegree/ivan/spotifystreamer/SearchFragment.java
+++ app/src/main/java/com/nanodegree/ivan/spotifystreamer/SearchFragment.java
@@ -1,6 +1,6 @@
 package com.nanodegree.ivan.spotifystreamer;
 
-import android.net.Uri;
+
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -12,23 +12,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
+import kaaes.spotify.webapi.android.models.Pager;
 
 
 /**
@@ -38,6 +32,8 @@ public class SearchFragment extends Fragment {
 
     private ArrayAdapter<String> mSpotifyAdapter;
 
+    EditText inputSearch;
+
     public SearchFragment() {
     }
 
@@ -46,157 +42,109 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        doSearch();
+        //doSearch();
+        List<String> weekForecast = new ArrayList<String>();
+
+        // Now that we have some dummy forecast data, create an ArrayAdapter.
+        // The ArrayAdapter will take data from a source (like our dummy forecast) and
+        // use it to populate the ListView it's attached to.
+        mSpotifyAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_spotify, // The name of the layout ID.
+                        R.id.list_item_spotify_textview, // The ID of the textview to populate.
+                        weekForecast);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_spotify);
+        listView.setAdapter(mSpotifyAdapter);
+
+        FetchSpotyArtistTask spotify = new FetchSpotyArtistTask();
+        spotify.execute("Dire Straits");
+        try {
+            doSearch(rootView);
+        }catch (Exception e)
+        {
+            Log.e("ERRRRRRRORRRRRRRR", e.toString() + " " + e.getMessage());
+
+        }
+
         return rootView;
 
     }
 
-
-
-
-
-    private void doSearch() {
-        final EditText et = (EditText)getActivity().findViewById(R.id.searchListSongs);
-        et.addTextChangedListener(new TextWatcher() {
+    private void doSearch(View rootView) {
+        inputSearch = (EditText)rootView.findViewById(R.id.searchListSongs);
+        /*inputSearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                String text = et.getText().toString().toLowerCase(Locale.getDefault());
+                String text = inputSearch.getText().toString().toLowerCase(Locale.getDefault());
+
                 // TODO lanzar busqueda lanzando la llamada AsyncTask
+                FetchSpotyArtistTask spotify = new FetchSpotyArtistTask();
+                spotify.execute(text);
+            }
+        });*/
+
+        inputSearch.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+
+                }else {
+                    FetchSpotyArtistTask spotify = new FetchSpotyArtistTask();
+                    spotify.execute(inputSearch.getText().toString());
+                }
             }
         });
     }
 
-    public class FetchSpotyArtistTask extends AsyncTask<String, Void, String[]> {
+    public class FetchSpotyArtistTask extends AsyncTask<String, Void,  String[]> {
 
         private final String LOG_TAG = FetchSpotyArtistTask.class.getSimpleName();
 
         /* The date/time conversion code is going to be moved outside the asynctask later,
              * so for convenience we're breaking it out into its own method now.
              */
+        private String getDataFromArtist(Artist artista){
+            return artista.name;
+        }
 
-
-        protected String[] doInBackground(String... params) {
-            // If there's no zip code, there's nothing to look up.  Verify size of params.
-            if (params.length == 0) {
-                return null;
-            }
-
+        @Override
+        protected  String[] doInBackground(String... params) {
             SpotifyApi api = new SpotifyApi();
             SpotifyService spotify = api.getService();
-            ArtistsPager results = spotify.searchArtists("Beyonce");
-
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            String format = "json";
-            String units = "metric";
-            int numDays = 7;
-
-            try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                final String FORECAST_BASE_URL =
-                        "http://api.openweathermap.org/data/2.5/forecast/daily?";
-                // id instead q in order to get my city in europe
-                final String QUERY_PARAM = "id";
-                final String FORMAT_PARAM = "mode";
-                final String UNITS_PARAM = "units";
-                final String DAYS_PARAM = "cnt";
-
-                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM, params[0])
-                        .appendQueryParameter(FORMAT_PARAM, format)
-                        .appendQueryParameter(UNITS_PARAM, units)
-                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                        .build();
-
-                URL url = new URL(builtUri.toString());
-
-                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-
-                Log.v(LOG_TAG, "Forecast string: " + forecastJsonStr);
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
+            ArtistsPager results = spotify.searchArtists(params[0]);
+            Pager<Artist> artist =  results.artists;
+            List<Artist> lista = artist.items;
+            // Todo recorrer la lista para coger .name , images, y lo que necesitemos
+            String[] resultStrs = new String[lista.size()];
+            int index = 0;
+            for (Artist artista: lista) {
+                resultStrs[index] = getDataFromArtist(artista);
+                index++;
             }
-
-            //try {
-            //    return null;
-                //} catch (JSONException e) {
-                //   Log.e(LOG_TAG, e.getMessage(), e);
-                //    e.printStackTrace();
-                //}
-
-             //This will only happen if there was an error getting or parsing the forecast.
-            return null;
+            Log.v(LOG_TAG, "Spotify string: " + lista.toString());
+            return resultStrs;
         }
 
         @Override
         protected void onPostExecute(String[] result) {
             if (result != null) {
                 mSpotifyAdapter.clear();
-                for(String artistStr : result) {
-                    mSpotifyAdapter.add(artistStr);
+                for(String albumSpotifyStr : result) {
+                    mSpotifyAdapter.add(albumSpotifyStr);
                 }
                 // New data is back from the server.  Hooray!
             }
         }
+
 
 
     }
